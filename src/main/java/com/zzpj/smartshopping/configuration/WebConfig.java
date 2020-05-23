32bae2f13d58287be54fe24e9c482b5afe40fbd4
dawidpytka <44122759+dawidpytka@users.noreply.github.com>
2020-05-23 17:49:19 +0200

    fix CORS

--- src/main/java/com/zzpj/smartshopping/configuration/WebConfig.java
+++ src/main/java/com/zzpj/smartshopping/configuration/WebConfig.java
@@ -6,13 +6,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-    @Configuration
-    @EnableWebMvc
-    public class WebConfig implements WebMvcConfigurer {
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
 
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**");
-        }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST","PUT","DELETE");
     }
+}
+
 
