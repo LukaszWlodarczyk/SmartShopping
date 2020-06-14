f9d5a58f245e60be6577bd4ba07d530648d0143b
Kuba Grebowicz <216770@edu.p.lodz.pl>
2020-06-14 20:01:13 +0200

    Add happyPath addOffer test case

--- src/test/java/com/zzpj/smartshopping/controllers/OfferControllerTest.java
+++ src/test/java/com/zzpj/smartshopping/controllers/OfferControllerTest.java
@@ -1,5 +1,66 @@
+package com.zzpj.smartshopping.controllers;
 import static org.junit.jupiter.api.Assertions.*;
+import com.zzpj.smartshopping.model.Offer;
+import com.zzpj.smartshopping.repositories.*;
+import com.zzpj.smartshopping.services.AllegroService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class OfferControllerTest {
+    @Mock
+    private AllegroService allegroService = mock(AllegroService.class);
+    private CategoryRepository categoryRepository = mock(CategoryRepository.class);
+    private OfferRepository offerRepository = mock(OfferRepository.class);
+    @InjectMocks
+    private OfferController offerController = new OfferController();
 
+    @Test
+    void addOffer_newOffer_shouldAddOffer() {
+        MockitoAnnotations.initMocks(this);
+        ResponseEntity<Offer> responseEntity;
+
+        Offer offer = new Offer(123L,
+                "url",
+                "name",
+                "dispName",
+                500,
+                4,
+                "Elektronika");
+
+        Optional<Offer> offerOptional = Optional.empty();
+
+        ReflectionTestUtils.setField(offer, "isGoodPrice", false);
+        ReflectionTestUtils.setField(offer, "isFavourite", false);
+        ReflectionTestUtils.setField(offer, "expectedPrice", 400);
+        ReflectionTestUtils.setField(offerController, "categoryRepository", categoryRepository);
+
+        when(allegroService.getSearchedOfferFromAllegro(any(), any(), any(), any()))
+                .thenReturn(new Offer(123L,
+                        "url",
+                        "name",
+                        "dispName",
+                        500,
+                        4,
+                        "Elektronika"));
+
+        when(offerRepository.findById(any())).thenReturn(offerOptional);
+
+        responseEntity = offerController.addOffer(125L,
+                "url",
+                "searchedPhrase",
+                "dispName",
+                400);
+
+        assertEquals(offer,responseEntity.getBody());
+    }
 }
